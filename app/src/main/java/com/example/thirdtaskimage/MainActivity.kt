e02e05cb895da03d55cf5fe3312695d8f71ad2a2
MariaRiya <pikuleva1@uniba.sk>
2021-11-06 16:52:30 +0500

    First commit

---
+++ app/src/main/java/com/example/thirdtaskimage/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.thirdtaskimage
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun loadImage(view: View) {
+        val uri_webPage = Uri.parse(website_edittext.text.toString())
+         Glide
+                .with(this)
+                .load(uri_webPage)
+                .listener(object : Any(),
+                    RequestListener<Drawable> {
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                       return false
+                    }
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        Toast.makeText(applicationContext, "Something went wrong. Try again, please.", Toast.LENGTH_LONG).show()
+                        return false
+                    }
+                })
+             .into(imageView)
+
+
+    }
+}
